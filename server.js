b3153b1d2ced4b70384f0b776d4d91a1be4b9edd
DGBaozzz1 <baoios098@gmail.com>
2025-07-16 21:31:07 +0700

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,9 @@ const PORT = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 app.use(express.static('public'));
-
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/index.html");
+});
 const dbFile = './data.json';
 let db = { keys: {}, bannedIPs: {} };
 
